b2cfa05e6bb9109f5ea742c63c5478708daae88a
timothyhsu8 <timothy.hsu@stonybrook.edu>
2021-10-11 09:33:05 -0400

    Set up basic socket functionality with Socket IO. Start by doing npm install -> npm start -> go into browser and type localhost:3000

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // io.emit broadcasts this message to all other users connected to the server
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on PORT:3000');
+});
