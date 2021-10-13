ffe8737d34019d856f48c1a861f69d67961bdaca
meetAlexLau <alex.lau.1@stonybrook.edu>
2021-10-13 00:05:20 -0400

    serverside Users list. bug=go somewhere event

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const users = {};
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg); // io.emit broadcasts this message to all other users connected to the server
-  });
+  if(!users[socket.id]){
+    users[socket.id] = [socket.id, " has entered the server"];
+    socket.emit('entered', users);
+  }
 });
+
+io.on('go somewhere', (data) =>{
+  socket.emit('go somewhere', data);
+})
 
 server.listen(3000, () => {
   console.log('listening on PORT:3000');
