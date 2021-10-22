76f793cdf5bf976b4adf3ea73853274cae686a72
timothyhsu8 <timothy.hsu@stonybrook.edu>
2021-10-22 19:12:13 -0400

    Connected MongoDB database to the backend. Every time a user connects, a new entry is added to the database. Todo -> Store their location as well and make sure it's consistently updated

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const app = express();
 const http = require('http');
-const server = http.createServer(app);
 const { Server } = require("socket.io");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const User = require('./UserSchema');
+
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(bodyParser.json({limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
 const users = {};
+
+const CONNECTION_URL = 'mongodb+srv://timhsu:7xvPjvAEI3jMuhhf@users.xnee2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+const MONGOOSE_PORT = 5001
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -18,9 +31,17 @@ io.on('connection', (socket) => {
     socket.on('go somewhere', function(users) {
       io.sockets.emit('go somewhere', users);
     })
+
+    mongoose.connect(CONNECTION_URL, { useNewUrlparser: true, useUnifiedTopology: true })
+
+    let newUser = new User({ name: socket.id, location: "Place" })
+    newUser.save()
+    // .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    // .catch((error) => console.log(error.message))
   }
 });
 
-server.listen(3000, () => {
-  console.log('listening on PORT:3000');
+
+server.listen(PORT, () => {
+  console.log('listening on PORT:5000');
 });
