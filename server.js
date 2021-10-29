c61bd9ca70c67a7073fd2918cb7f7af8d99ff4aa
timothyhsu8 <timothy.hsu@stonybrook.edu>
2021-10-29 15:06:04 -0400

    Changed recipe to mean

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.get('/group/:groupId', (req, res) => {
+  res.send("Welcome Group " + req.params.groupId + "!")
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
