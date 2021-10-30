eb75906f9749ef90c9895ae1e8f145b67c21ced0
timothyhsu8 <timothy.hsu@stonybrook.edu>
2021-10-29 22:15:39 -0400

    Added axios. Added king of the hill feature to test shared state

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const User = require('./server/UserSchema');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
 app.use(bodyParser.json({limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
@@ -17,26 +16,33 @@ app.use(cors())
 const router = require('./server/router.js')
 app.use('/api', router)
 
-const users = {};
-
 const CONNECTION_URL = 'mongodb+srv://timhsu:7xvPjvAEI3jMuhhf@users.xnee2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
-const MONGOOSE_PORT = 5001
+
+const users = {};
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// Socket event for someone new connecting to the server
 io.on('connection', (socket) => {
   if(!users[socket.id]){
     users[socket.id] = [socket.id, " has entered the server"];
+    
+    // Socket event for user entering the server
     io.sockets.emit('entered', users);
+    
+    // Socket event for user going somewhere
     socket.on('go somewhere', function(users) {
       io.sockets.emit('go somewhere', users);
     })
 
-    mongoose.connect(CONNECTION_URL, { useNewUrlparser: true, useUnifiedTopology: true })
 
+    // Connection to MongoDB
+    mongoose.connect(CONNECTION_URL, { useNewUrlparser: true, useUnifiedTopology: true })
+    
+    // Save user to database
     let newUser = new User({ name: socket.id, location: "PlaceUpdated" })
     // newUser.save()
     // .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
@@ -46,5 +52,5 @@ io.on('connection', (socket) => {
 
 
 server.listen(PORT, () => {
-  console.log('listening on PORT:5000');
+  console.log(`Server listening on port ${PORT}`);
 });
