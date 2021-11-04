a48ecfa747f7ac5f01d60f82ab1a325ccb824ef4
timothyhsu8 <timothy.hsu@stonybrook.edu>
2021-11-03 21:01:42 -0400

    Initial commit

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000
+const http = require("http")
+const server = http.createServer(app)
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+  res.sendFile(__dirname + '/index.html');
+});
 
-app.get('/group/:groupId', (req, res) => {
-  res.send("Welcome Group " + req.params.groupId + "!")
-})
+// app.get('/group/:groupId', (req, res) => {
+//   res.send("Welcome Group " + req.params.groupId + "!")
+// })
 
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+server.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
 })
