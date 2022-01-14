8629cea00bca76796c1071cfd519a5eae265115b
Almondbread <pstdenis@gmail.com>
2022-01-14 06:49:16 +0000

    working html<->twee

--- tweeGaze.js
+++ tweeGaze.js
@@ -1,33 +1,45 @@
-var gaze = require('gaze');
-const { exec } = require('child_process');
+let gaze = require('gaze');
+let coolDown = 0;
+const fs = require('fs');
+const {
+    exec
+} = require('child_process');
+const e = require('express');
 
 // Watch all .js files/dirs in process.cwd() 
-gaze('Twine/*.*', function(err, watcher) {
+gaze('Twine/*.*', function (err, watcher) {
     // Files have all started watching 
-    // watcher === this 
+    // watcher === this
+
+    
     console.log('Watching files...');
 
     // Get all watched files 
     var watched = this.watched();
-console.log(watched)
+    console.log(watched)
     // On file changed 
-    this.on('changed', function(filepath) {
+    this.on('changed', function (filepath) {
+   
         [suffix, ...prefix] = filepath.split(".").reverse()
-        prefix=prefix.reverse().join(".")
-     let command;
+        prefix = prefix.reverse().join(".")
+        let command;
 
         if (suffix == "html") {
+
+
             command = `tweego -f sugarcube-2 -d -o ${prefix}.twee ${prefix}.html`
-      
-        } else if (suffix == "twee"){
+
+        } else if (suffix == "twee") {
             command = `tweego -f sugarcube-2  ${prefix}.twee -o ${prefix}.html`
-        }
-        else {
+        } else {
             console.log(prefix, suffix)
             return
         }
-            // Executes shell command to convert .html file to .twee file
-            exec(command, (err, stdout, stderr) => {					// tweego -d -o index.twee index.html
+        const mtime = fs.statSync(filepath).mtime;
+        if (mtime - coolDown > 1000) {
+            coolDown = mtime
+           // Executes shell command 
+            exec(command, (err, stdout, stderr) => { // tweego -d -o index.twee index.html
                 if (err) {
                     //some err occurred
                     console.error(err)
@@ -36,9 +48,10 @@ console.log(watched)
                     // console.log(`stdout: ${stdout}`);
                 }
             });
-       
 
-        console.log(filepath + ' was changed');
+
+            console.log(filepath + ' was changed');
+        }
     });
 
     // Get watched files with relative paths 
