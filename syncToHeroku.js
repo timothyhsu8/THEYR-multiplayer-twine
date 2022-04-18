6d4eb3d5ee1137170294e028e46129d52817517d
timothyhsu8 <timothy.hsu@stonybrook.edu>
2022-04-18 08:55:01 -0400

    Automated update to Heroku/Github

--- syncToHeroku.js
+++ syncToHeroku.js
@@ -2,19 +2,19 @@ import { execSync } from 'child_process';
 
 let herokuInstances = 5
 
+let commands = [
+    `git add .`,
+    `git commit -m "Automated update to Heroku/Github"`
+]
+
 for (let i = 1; i <= herokuInstances; i++) {
-    let commands = [
-        `git add .`,
-        `git commit -m "Automated update to Heroku/Github"`,
-        `git push`,
-        `echo git push https://git.heroku.com/aztec-${i}.git HEAD:master`
-    ]
-    
+    commands.push(`echo git push https://git.heroku.com/aztec-${i}.git HEAD:master`);
+
     for (let command of commands) {
         try {
             execSync(command, console.log);
         } catch(err) {}
         console.log(commands);
     }
-    commands = [commands.pop()];
+    commands = [];
 }
