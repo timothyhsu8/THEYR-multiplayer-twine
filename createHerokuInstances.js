8700fe9df507cab8458934e645722ee30a8e1b38
timothyhsu8 <timothy.hsu@stonybrook.edu>
2022-04-20 12:15:54 -0400

    Added separate config file for heroku instances. Added error checking for discord nickname.

--- createHerokuInstances.js
+++ createHerokuInstances.js
@@ -3,24 +3,28 @@ import { createRequire } from "module";
 import { exit } from 'process';
 const require = createRequire(import.meta.url);
 // const { clientId, clientSecret, twinePath, port } = require('./loginDiscord/config.json')
-const { ...configVars } = require('./loginDiscord/config.json')
+// const { ...configVars } = require('./loginDiscord/config.json')
+const configArray = require('./loginDiscord/configHerokuInstances.json');
 
 
 let herokuInstances = 5
-let redirectURL = configVars['redirectURL'];
 for (let i = 1; i <= herokuInstances; i++) {
+    let configVars = configArray.shift();
+    
+    // Deploy Application
     let commands = [
         `heroku apps:destroy -a aztec-${i} --confirm aztec-${i}`,
         `heroku create -a aztec-${i} --buildpack heroku/nodejs`, 
-        // `heroku buildpacks:set heroku/nodejs`,
         `heroku buildpacks:add -a aztec-${i} heroku-community/multi-procfile`,
         `heroku config:set -a aztec-${i} PROCFILE=Procfile`, 
         `git push https://git.heroku.com/aztec-${i}.git HEAD:master`
     ]
 
-    configVars['herokuURL'] = `https://aztec-${i}.herokuapp.com/`;
+    
+    let redirectURL = configVars['redirectURL'];
     configVars['redirectURL'] = redirectURL.replace("http%3A%2F%2Flocalhost%3A53134", encodeURIComponent(`https://aztec-${i}.herokuapp.com/`)).replace(/&/g, '"&"');
-    console.log(configVars['redirectURL']);
+    
+    // Set config variables on Heroku
     for (let key of Object.keys(configVars)) {
         let command = `heroku config:set -a aztec-${i} ${key}=${configVars[key]}`;
         console.log(command);
@@ -32,5 +36,4 @@ for (let i = 1; i <= herokuInstances; i++) {
             execSync(command, console.log);
         } catch(err) {}
     }
-    console.log(herokuInstances)
 }
