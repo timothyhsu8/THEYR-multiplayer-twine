5351ea4c2e04c9f0056d0049df5220d59e5a431a
timothyhsu8 <timothy.hsu@stonybrook.edu>
2022-04-27 10:35:59 -0400

    Removed some unnecessary files.

--- Twine/script.js
+++
@@ -1,9 +0,0 @@
-let users= SugarCube.State.getVar("$users") || {};
-console.log({userId},users)
-if(!(userId  in users))
-{
-    users[userId]={}
-    SugarCube.State.setVar("$users",users);
-}
-
-SugarCube.Engine.show();
